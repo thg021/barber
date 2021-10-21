74baea8d2e9e82cfb3a064655d36b9b4154bec67
Thiago Silva <thiago.silva@hotmail.com>
2021-10-21 20:44:32 -0300

    feat: add migration user

---
+++ src/database/migrations/1634859548038-CreateUsers.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUsers1634859548038 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
